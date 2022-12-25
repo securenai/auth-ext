90273b161dc0ae46b79874cee3e1063fb1061263
Michael Cheng <michaelnccu@gmail.com>
2022-12-26 01:37:31 +0800

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
+
+@Module({
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
